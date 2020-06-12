e7a3ffb2a67cab8f2555a2797db5ae8bf91a0b60
Frank Mwandi <frankmwandi93@gmail.com>
2020-06-12 09:31:58 -0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <img src="https://media3.giphy.com/media/3o85xCVh0TmBrQr36U/200w.webp?cid=ecf05e478f2c1724915c8c6c588d79ddb02ca2817a1bad80&rid=200w.webphttps://media0.giphy.com/media/EDV30lQQ9VW5q/giphy.webp?cid=ecf05e47155d666cb9308d1c522836fc7cd6ec2d294c4077&rid=giphy.webp" class="uk-position-center" alt="">
+  </div>
+</template>
+
+<script>
+
+
